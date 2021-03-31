b18a647901122ae227f4ebe3f48a86e873ba7ad8
Dhiogo <dhiogo.correa@itera.com.br>
2021-03-31 20:33:41 -0300

    new color

--- src/core/utils/site_variables_vania.js
+++ src/core/utils/site_variables_vania.js
@@ -7,7 +7,7 @@ export const subtitle = () => {
 };
 
 export const cardBgColor = () => {
-  return '#fff5e1';
+  return '#A9C5A8';
 };
 
 export const families = () => {
@@ -18,7 +18,7 @@ export const families = () => {
       description:
         'Descendentes de portugueses da região Filgueiras, do norte de Portugal que se fixaram em São José Del Rei (atual Tiradentes) em Minas Gerais. ',
       image: 'https://carneiro-flavio-honorio.s3.amazonaws.com/brasao_carneiro.jpg',
-      pdfUrl: 'https://carneiro-flavio-honorio.s3.amazonaws.com/Fam%C3%ADlias+Carneiro.pdf',
+      pdfUrl: 'https://carneiro-flavio-honorio.s3.amazonaws.com/Fam%C3%ADlias+Carneiro_compressed.pdf',
     },
     {
       key: 2,
@@ -26,7 +26,7 @@ export const families = () => {
       description:
         'Originários de Ilha Graciosa, Arquipélago dos Açores, Portugal, após breve passagem por Rio Grande do Sul (RS), fixaram-se em São João Del Rey (MG), atual Tiradentes. ',
       image: 'https://carneiro-flavio-honorio.s3.amazonaws.com/brasao_flavio.png',
-      pdfUrl: 'https://carneiro-flavio-honorio.s3.amazonaws.com/Fam%C3%ADlias+Fl%C3%A1vio.pdf',
+      pdfUrl: 'https://carneiro-flavio-honorio.s3.amazonaws.com/Fam%C3%ADlias+Fl%C3%A1vio_compressed.pdf',
     },
     {
       key: 3,
@@ -34,7 +34,7 @@ export const families = () => {
       description:
         'Desconhece-se suas origens mas fixaram-se em Dores de Santa Juliana, distrito de Araxá, em meados do século XIX. ',
       image: 'https://carneiro-flavio-honorio.s3.amazonaws.com/brasao_honorio.jpg',
-      pdfUrl: 'https://carneiro-flavio-honorio.s3.amazonaws.com/Fam%C3%ADlias+Hon%C3%B3rio.pdf',
+      pdfUrl: 'https://carneiro-flavio-honorio.s3.amazonaws.com/Fam%C3%ADlias+Hon%C3%B3rio_compressed.pdf',
     },
   ];
 };
